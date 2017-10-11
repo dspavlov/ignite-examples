9a41a20e4d5f5cbe9d0bd3fb4931c725ed6a29a4
dpavlov <dpavlov@gridgain.com>
2017-10-11 14:17:53 +0300

    demo mass load

--- src/main/java/ClientNode.java
+++ src/main/java/ClientNode.java
@@ -2,4 +2,7 @@
  * Created by dpavlov on 11.10.2017.
  */
 public class ClientNode {
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/ServerNode.java
+++ src/main/java/ServerNode.java
@@ -2,4 +2,7 @@
  * Created by dpavlov on 11.10.2017.
  */
 public class ServerNode {
+    public static void main(String[] args) {
+
+    }
 }
