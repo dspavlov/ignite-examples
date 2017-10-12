9450b44fbf687f5bd64ad0ac6b25e78c9a7b9e3a
dpavlov <dpavlov@gridgain.com>
2017-10-12 17:58:40 +0300

    entry processor invoke and invoke all

--- src/main/java/ClientNode.java
+++ src/main/java/ClientNode.java
@@ -1,8 +1,54 @@
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import javax.cache.Cache;
+import javax.cache.processor.EntryProcessor;
+import javax.cache.processor.EntryProcessorResult;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheEntry;
+import org.apache.ignite.cache.CacheEntryProcessor;
+import org.apache.ignite.configuration.IgniteConfiguration;
+
 /**
  * Created by dpavlov on 11.10.2017.
  */
 public class ClientNode {
     public static void main(String[] args) {
+        final IgniteConfiguration cfg = new IgniteConfiguration();
+        cfg.setClientMode(true);
+        try (final Ignite ignite = Ignition.start(cfg)) {
 
+            final IgniteCache<Integer, String> test = ignite.getOrCreateCache("EntryTest");
+            test.putAll(new TreeMap<Integer, String>() {{
+                put(1, "hi");
+                put(2, "there");
+                put(3, "!");
+            }});
+
+            CacheEntryProcessor<Integer, String, Integer> ep = (entry, parms) -> {
+                String v = entry.getValue();
+                entry.setValue("_" + v + "_");
+                System.err.println("Running entry processor");
+                return v.length();
+            };
+            final Integer invoke = test.invoke(1, ep);
+
+
+
+            Set<Integer> keys = new HashSet<>();
+            for (Cache.Entry<Integer, String> e : test) {
+                keys.add(e.getKey());
+            }
+            final Map<Integer, EntryProcessorResult<Integer>> map = test.invokeAll(keys, ep);
+            System.out.println(map);
+
+            for (Cache.Entry<Integer, String> e : test) {
+                System.out.println(e);
+            }
+            System.err.println(invoke);
+        }
     }
 }
--- src/main/java/ServerNode.java
+++ src/main/java/ServerNode.java
@@ -1,8 +1,15 @@
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.configuration.IgniteConfiguration;
+
 /**
  * Created by dpavlov on 11.10.2017.
  */
 public class ServerNode {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        final IgniteConfiguration cfg = new IgniteConfiguration();
+        try (final Ignite start = Ignition.start(cfg)) {
+            Thread.sleep(1000000);
+        }
     }
 }
