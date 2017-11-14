1e72f6e91a1f4e53c09ae42e86fa7d3488e9890b
dpavlov <dpavlov@gridgain.com>
2017-11-14 19:36:05 +0300

    initial load, snapshots

---
+++ src/main/java/Account.java
@@ -0,0 +1,14 @@
+/**
+ *
+ */
+public class Account {
+
+    private final int id;
+    private final String name;
+
+    public Account(int id, String name) {
+
+        this.id = id;
+        this.name = name;
+    }
+}
